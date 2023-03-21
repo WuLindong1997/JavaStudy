7890d623c29bce5e46a301a73580567648a38440
wld <806699882@qq.com>
2023-03-21 08:55:02 +0800

    Initial commit

---
+++ src/test01.java
@@ -0,0 +1,31 @@
+public class test01 {
+    public static void main(String[] args){
+        int a = 100;
+        double b = 101.1;
+        float c = 102.5F;
+        int d =1,f=2,g=3,h=10 ;
+        byte k= 1;
+        short l = 2;
+        long ll = 11211111;
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(ll);
+        /*
+        byte  1     -128  to  127
+        short 2
+        int   4
+        long  8
+
+        float 4
+        double 8
+
+        char  2
+
+        boolean 1
+         */
+    }
+}
